558c176d8a2316f7bdcd78ae076e0e4c7439587c
Tudor Hulban <prindex2010@gmail.com>
2026-03-24 19:06:08 +0200

    zerolog benchmark

--- logger_log_context_bench_test.go
+++ logger_log_context_bench_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/require"
 	"github.com/tudorhulban/log/bytearena"
 	"github.com/tudorhulban/log/helpers"
@@ -97,5 +98,29 @@ func BenchmarkContext_With_JSON(b *testing.B) {
 
 		// 2. Print
 		entry.Print("benchmark test")
+	}
+}
+
+// BenchmarkZerolog_WithFields-16    	 5704693	       215.3 ns/op	       0 B/op	       0 allocs/op
+func BenchmarkZerolog_WithFields(b *testing.B) {
+	var writer helpers.NoopWriter
+
+	logger := zerolog.New(&writer).With().
+		Timestamp().
+		Str("service", "auth").
+		Int("req_id", 12345).
+		Bool("cache_hit", true).
+		Logger()
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; b.Loop(); i++ {
+		logger.Info().
+			Str("area", "some area").
+			Str("user", "tudor").
+			Int("attempt", i).
+			Bool("success", true).
+			Msg("benchmark test")
 	}
 }
